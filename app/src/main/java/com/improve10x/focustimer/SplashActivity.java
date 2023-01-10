8e0c9619eac965126c6fa5d755583394ab96b438
Renuka Challa <challarenuka20@gmail.com>
2023-01-10 17:27:01 +0530

    Xml design

--- app/src/main/java/com/improve10x/focustimer/SplashActivity.java
+++ app/src/main/java/com/improve10x/focustimer/SplashActivity.java
@@ -2,9 +2,16 @@ package com.improve10x.focustimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,10 +20,22 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
+        FirebaseApp.initializeApp(/*context=*/ this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+                PlayIntegrityAppCheckProviderFactory.getInstance());
         Handler handler = new Handler();
         handler.postDelayed(
                 () -> {
-                    Toast.makeText(this, "Welcome to Pomodoro app!", Toast.LENGTH_SHORT).show();
+
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    if (user != null) {
+                        Toast.makeText(this, "Welcome to Pomodoro app!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Intent intent = new Intent(this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
                 }, 3000
         );
     }
